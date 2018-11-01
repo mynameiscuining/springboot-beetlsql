9b7a23d4b1a9de4bad5e5587b71cfd406b836287
cuining <2387850043@qq.com>
2018-11-01 16:00:57 +0800

    添加代理服务

--- src/main/java/cn/njyazheng/demo/config/DataSourceConfig.java
+++ src/main/java/cn/njyazheng/demo/config/DataSourceConfig.java
@@ -1,14 +1,18 @@
 package cn.njyazheng.demo.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.mitre.dsmiley.httpproxy.ProxyServlet;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
 @Configuration
-public class DataSourceConfig {
+public class DataSourceConfig{
     /**
      * 数据源配置
      * @param
@@ -19,5 +23,20 @@ public class DataSourceConfig {
     public DataSource dataSource(){
         HikariDataSource dataSource=new HikariDataSource();
         return dataSource;
+    }
+
+    /**
+     * 代理配置
+     * @param environment
+     * @return
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean(Environment environment){
+        ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new ProxyServlet(), environment.getProperty("proxy.access_url"));
+        //这个setName必须要设置，并且多个的时候，名字需要不一样
+        servletRegistrationBean.setName("proxy");
+        servletRegistrationBean.addInitParameter("targetUri", environment.getProperty("proxy.target_url"));
+        servletRegistrationBean.addInitParameter(ProxyServlet.P_LOG, environment.getProperty("proxy.logging_enabled"));
+        return servletRegistrationBean;
     }
 }
